25994f6c8e6b2064ff2d424b049889ce9ec387c1
XPNguyenDL <xpnguyen240802@gmail.com>
2023-03-03 22:48:47 +0700

    Fix Bug

--- src/components/Layout/Header.jsx
+++ src/components/Layout/Header.jsx
@@ -2,25 +2,16 @@ import Navbar from "./components/Navbar";
 import Link from "next/link";
 import Image from "next/image";
 import Logo from "@/public/assets/icons/neko-logo.svg";
+import Sidebar from "./components/Sidebar";
 
 export default function Head() {
-
-  
-  
-
   return (
-    <div className="bg-primary-100 drop-shadow">
-      <div className=" container px-4 mx-auto flex flex-wrap items-center justify-between">
+    <div className="flex fixed top-0 left-0 w-full h-[var(--header-height)] bg-secondary-100 drop-shadow">
+        <Sidebar />
+      <div className="container px-4 h-[var(--header-height)] mx-auto flex flex-wrap items-center justify-end xl:justify-between">
         <div className="lg:w-auto lg:static lg:block lg:justify-start">
-          <Link
-            className="inline-block"
-            href="/"
-          >
-            <Image
-              src={Logo}
-              className="max-w-[90px]"
-              alt="brand-logo"
-            />
+          <Link className="inline-block" href="/">
+            <Image src={Logo} className="mt-[2px] max-w-[90px] max-h-[60px] object-cover" alt="brand-logo" />
           </Link>
         </div>
         <Navbar />
--- src/components/Layout/components/Navbar.jsx
+++ src/components/Layout/components/Navbar.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineMenu } from "react-icons/ai";
 import { useEffect, useLayoutEffect, useState } from "react";
 
 const pages = [
@@ -29,61 +30,30 @@ const pages = [
 
 export default function Navbar() {
   const NAVIGATION_ANIMATION =
-    "transition transform duration-500 ease-in-out hover:-translate-y-1";
-
-  /**
-   * Component's event handlers
-   */
-  const [mobileMode, setMobileMode] = useState(true);
-  const [isClosing, setIsClosing] = useState(false);
-  /**
-   * Component's event handlers
-   */
-  const handleOpenMobileNavbar = () => {
-    setIsClosing(false);
-    setMobileMode(false);
-   
-  };
-
-  useEffect(() => {
-    
-    window.onresize = () => {
-      console.log(window.innerWidth < 1280);
-      setMobileMode(window.innerWidth <= 1280);
-    };
-  }, []);
-
-  useEffect(() => {
-      setMobileMode(window.innerWidth <= 1280);
-  }, []);
+    "transition transform duration-500 slide-in hover:-translate-y-1 ";
 
   return (
     <>
-      <nav className="relative flex flex-wrap items-center justify-between px-2 mt-1 mb-1" >
+      <nav className="relative flex flex-wrap items-center justify-between px-2 mt-1 mb-1">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-          {!mobileMode ? (
-            <div className="lg:flex items-center" id="example-navbar-danger">
-              <ul className="flex lg:flex-row list-none lg:ml-auto">
-                {pages.map((page, index) => (
-                  <li className="nav-item" key={index}>
-                    <Link
-                      className={
-                        "p-3 flex items-center text-xs uppercase font-semibold border-opacity-0 border-b border-gray hover:border-opacity-100 hover:opacity-75" +
-                        NAVIGATION_ANIMATION
-                      }
-                      href="/"
-                    >
-                      <span className="font text-lg">{page.name}</span>
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ) : null}
-          {mobileMode ? (
-            <></>
-          ) : null}
-        </div>
+          <div className="lg:flex items-center" id="example-navbar-danger">
+            <ul className="hidden xl:flex lg:flex-row list-none lg:ml-auto">
+              {pages.map((page, index) => (
+                <li className="nav-item" key={index}>
+                  <Link
+                    className={
+                      "p-3 flex items-center text-xs text-black uppercase font-semibold border-opacity-0 border-b border-gray hover:border-opacity-100 opacity-75 hover:opacity-100 hover:text-purple " +
+                      NAVIGATION_ANIMATION
+                    }
+                    href="/"
+                  >
+                    <span className="font text-lg">{page.name}</span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          </div>
       </nav>
     </>
   );
