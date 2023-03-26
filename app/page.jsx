a5f0992b743bd60ab5c1d33663b10af33a3c51c6
XPNguyenDL <xpnguyen240802@gmail.com>
2023-03-26 23:19:46 +0700

    Add Product & Description

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,19 @@
+import Description from "@/src/components/Home/Description";
 import CategorySection from "@/src/components/Home/CategorySection";
+import NewProduct from "@/src/components/Home/NewProduct";
 import Slider from "@/src/components/Slider/Slider";
+
+export const metadata = {
+  title: "Trang chủ",
+};
 
 export default function Home() {
   return (
     <div>
       <Slider/>
       <CategorySection />
+      <NewProduct/>
+      <Description />
     </div>
   );
 }
