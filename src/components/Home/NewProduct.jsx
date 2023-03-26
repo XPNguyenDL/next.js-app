a5c5007c144075e847e30f8f3097b9f8e37c5816
XPNguyenDL <xpnguyen240802@gmail.com>
2023-03-26 23:20:05 +0700

    Add product

---
+++ src/components/Home/NewProduct.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ProductItem from "./ProductItem";
+import SectionHeader from "./SectionHeader";
+
+export default function NewProduct() {
+  return (
+    <div className="mx-auto px-6 md:px-10 max-w-screen-2xl">
+      <SectionHeader title="Sách mới" />
+      <div className="container mx-auto">
+        <div class="grid grid-cols-2 md:grid-cols-3 md:grid-cols-4 gap-4">
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+            <ProductItem />
+        </div>
+      </div>
+    </div>
+  );
+}
