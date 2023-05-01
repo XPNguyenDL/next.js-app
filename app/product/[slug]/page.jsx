ee164f7c351743ed08dde5f92cc948678cfcd509
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-05-01 21:59:13 +0700

    Add notFoundException

--- app/product/[slug]/page.jsx
+++ app/product/[slug]/page.jsx
@@ -3,14 +3,19 @@ import PopularProducts from "@/src/components/Product/PopularProducts";
 import ProductSection from "@/src/components/Product/ProductSection";
 import UseReduce from "@/src/reducers/UseReduce";
 import React from "react";
+import { notFound } from 'next/navigation';
 
 export default async function page({ params }) {
   const productData = GetProductBySlug(params.slug);
   const data = await productData;
+  console.log(data);
+  if (!data.isSuccess) return notFound();
+
+
   return (
     <div className="mt-16">
       <UseReduce>
-        <ProductSection data={data} />
+        <ProductSection data={data.result} />
       </UseReduce>
       <PopularProducts />
     </div>
