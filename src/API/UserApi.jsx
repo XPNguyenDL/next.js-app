f4b0cf0131b0ae2f9bd97ec4b0f8802857c28e76
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-05-01 21:59:55 +0700

    Add users api

---
+++ src/API/UserApi.jsx
@@ -0,0 +1,21 @@
+import { URL_API } from "../components/Services/Store";
+
+export async function LoginApi(data) {
+  return fetch(`${URL_API}/api/accounts`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Lỗi gòi");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      return data;
+    })
+}
