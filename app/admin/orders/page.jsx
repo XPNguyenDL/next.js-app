ff158be0896d5671a3324251f5ba6237d4a7d6d8
Nguyễn Xuân Phát <2014478@dlu.edu.vn>
2023-05-02 22:06:16 +0700

    Add order page

--- app/admin/orders/page.jsx
+++ app/admin/orders/page.jsx
@@ -1,7 +1,13 @@
+import OrderManager from '@/src/components/Admin/Orders/OrderManager'
 import React from 'react'
 
 export default function page() {
   return (
-    <div className="mt-16">page</div>
+    <div className="mt-16 container mx-auto">
+      <div>
+        <h1 className='text-xl font-bold'>Thông tin đơn hàng</h1>
+      </div>
+      <OrderManager />
+    </div>
   )
 }
